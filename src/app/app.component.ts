272d9fc9305dbe98f73723244538afa3f795055d
William Daza <williamdaza04@gmail.com>
2023-06-07 11:49:19 -0500

    create login template and logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'ptecnicaangular';
+
+  constructor() {
+    const users = [
+      { username: 'usuario1', password: 'contraseña1' },
+      { username: 'usuario2', password: 'contraseña2' }
+    ];
+
+    localStorage.setItem('users', JSON.stringify(users));
+  }
 }
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.sass']
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  username: string = '';
+  password: string = '';
 
+  login() {
+    const usersLocalStorage = localStorage.getItem('users');
+    const users = usersLocalStorage ? JSON.parse(usersLocalStorage) : [];
+
+    const user = users.find((u: { username: string; password: string; }) => u.username === this.username && u.password === this.password);
+
+    if (user) {
+      console.log('Inicio de sesión exitoso');
+      // Aquí puedes redirigir al usuario a otra página
+    } else {
+      console.log('Nombre de usuario o contraseña incorrectos');
+    }
+  }
 }
