70055cbcb14fee5621a0379360670fceb10b02a3
William Daza <williamdaza04@gmail.com>
2023-06-07 11:58:04 -0500

    create components for the dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { UserInfoComponent } from './components/dashboard/user-info/user-info.component';
+import { FileUploadComponent } from './components/dashboard/file-upload/file-upload.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    DashboardComponent,
+    UserInfoComponent,
+    FileUploadComponent
   ],
   imports: [
     BrowserModule,
